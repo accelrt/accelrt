11fe1ec1643e5e72ca016c9bbf412acebf103913
Joel <58995538+jtpotato@users.noreply.github.com>
2025-10-06 12:51:15 +1100

    feat: update TeamMemberGrid role description and rename blur.css to overlay.css

--- src/app/TeamMemberGrid.tsx
+++ src/app/TeamMemberGrid.tsx
@@ -7,7 +7,7 @@ export default function TeamMemberGrid() {
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 p-4">
         <TeamMember
           name="Shuwei Guo"
-          role="Founder (placeholder image is used here, please replace)"
+          role="Founder"
           imageSrc="https://cdn-icons-png.flaticon.com/512/8136/8136031.png"
         />
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import TeamMemberGrid from "./TeamMemberGrid";
 import Header from "./Header";
 import ProspectusButton from "./Prospectus";
-import "./blur.css";
+import "./overlay.css";
 
 export default function Home() {
   return (
     <>
       {/* <Header /> */}
-      <main className="max-w-2xl m-auto linear-blur">
+      <main className="max-w-2xl m-auto linear-gradient">
         <div className="h-96 py-72 flex flex-col justify-center ">
           <h1 className="text-6xl font-bold text-center">AccelRT</h1>
           <a href="mailto:aushackathons@gmail.com" className="text-center">
